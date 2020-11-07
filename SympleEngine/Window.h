a8b15cb8c0bdcdbb8b67fdb5b64ad1c2cc831a07
Treidex <29633838+TeddyTelanoff@users.noreply.github.com>
2020-11-06 17:41:58 -0800

    5

---
+++ SympleEngine/Window.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "Core/Core.h"
+
+namespace Symple
+{
+	class SYMPLE_API Window
+	{
+		
+	};
+}
--- premake5.lua
+++ premake5.lua
@@ -9,13 +9,19 @@ workspace "SympleEngine"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+includeMap = {};
+includeMap["spdlog"] = "SympleEngine/vender/spdlog/include"
+includeMap["GLFW"] = "SympleEngine/vender/GLFW/include"
+
+include "SympleEngine/vender/GLFW"
+
 project "SympleEngine"
 	location "SympleEngine"
 	kind "SharedLib"
 	language "C++"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("int/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files {
 		"%{prj.name}/src/**.h",
@@ -23,11 +29,81 @@ project "SympleEngine"
 		"%{prj.name}/src/**.cpp"
 	}
 
-	include {
-		"%{prj.name}/vender/spdlog/include"
+	includedirs {
+		"%{prj.name}/src",
+		"%{includeMap.spdlog}",
+		"%{includeMap.GLFW}"
 	}
 
 	filter "system:windows"
 		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
 
-	SY_PLATFORM_WINDOWS;SY_BUILD_DLL;_WINDLL;
+		defines {
+			"SY_PLATFORM_WINDOWS",
+			"SY_BUILD_DLL"
+		}
+
+		postbuildcommands {
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/SympleExample")
+		}
+
+	filter "configurations:Debug"
+		defines "SY_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SY_RELEASE"
+		optimize "On"
+
+	filter "configurations:Production"
+		defines "SY_PRODUCTION"
+		optimize "On"
+
+project "SympleExample"
+	location "SympleExample"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.c",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs {
+		"%{includeMap.spdlog}",
+		"%{includeMap.GLFW}",
+		"SympleEngine/src"
+	}
+
+	links {
+		"SympleEngine",
+		"GLFW",
+		"opengl32.lib"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines {
+			"SY_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "SY_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SY_RELEASE"
+		optimize "On"
+
+	filter "configurations:Production"
+		defines "SY_PRODUCTION"
+		optimize "On"
